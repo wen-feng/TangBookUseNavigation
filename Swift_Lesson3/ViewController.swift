487284e81798b7fe207bd3519a90f5d04a0e0e2a
Wu, Weng-Feng <wuweng-feng@1505006nb01.local>
2016-06-27 19:23:14 +0800

    Initial Commit

---
+++ Swift_Lesson3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift_Lesson3
+//
+//  Created by Wu, Weng-Feng on 2016/6/27.
+//  Copyright © 2016年 Wu, Weng-Feng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
